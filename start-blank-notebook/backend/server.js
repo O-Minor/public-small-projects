f8f0389a3fb6120709921eae91040909d62e4007
Opal Oliver Minor <42308423+O-Minor@users.noreply.github.com>
2026-09-08 16:05:27 -0700

    better error messages and starting account sign up system in server.js

--- start-blank-notebook/backend/server.js
+++ start-blank-notebook/backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require('dotenv').config(); //import to be able to hide password in a file that isn't in git
-console.log(typeof process.env.DB_PASSWORD);
+// console.log(typeof process.env.DB_PASSWORD);
 
 const { Pool } = require("pg");
 const cors = require("cors");
@@ -38,10 +38,11 @@ app.get("/api/notes", async (req, res) => {
         res.json(result.rows);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Database error" });
+        res.status(500).json({ error: "Database error getting notes" });
     }
 });
 
+// post a note
 app.post("/api/notes", async (req, res) => {
     try {
         const { content } = req.body;
@@ -54,8 +55,50 @@ app.post("/api/notes", async (req, res) => {
         res.status(201).json(result.rows[0]);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Database error" });
+        res.status(500).json({ error: "Database error posting note" });
     }
+});
+
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({
+        error: "Username or password are empty",
+      });
+    }
+
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({
+        error: "Username already exists :(",
+      });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (username, password_hash)
+       VALUES ($1, $2)
+       RETURNING id, username`,
+      [username, passwordHash]
+    );
+
+    res.status(201).json({
+      message: "Account created :D",
+      user: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Database error creating account",
+    });
+  }
 });
 
 app.listen(port, () => {
